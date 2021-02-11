272d59db6ef7f41f51ecf77c77664f078d54b6d6
splashley <ashleyjlhynes@gmail.com>
2021-02-10 22:52:39 -0500

    added api call for search input

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,15 +1,76 @@
 <template>
-    <div>
-<input type="text" placeholder="testing 1 2 3"/>
-    </div>
+  <div class="autocomplete-list">
+    <input
+      type="text"
+      class="autocomplete-input"
+      placeholder="testing 1 2 3"
+      id="search-input"
+      v-model="searchValue"
+      @input="onChange"
+    />
+      <div
+        v-for="(results, index) in searchResults"
+        :key="index"
+        class="autocomplete-result"
+      >
+        <div class="">{{ results["1. symbol"] }}</div> 
+        <div class="">{{ results["2. name"] }}</div>
+      </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import axios from "axios";
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchValue: "",
+      searchResults: undefined,
+    };
+  },
+  methods: {
+    onChange() {
+      this.apiCall();
+    },
+    apiCall() {
+      const urlBase = `https://www.alphavantage.co/query`;
+      const params = {
+        function: "SYMBOL_SEARCH",
+        keywords: this.searchValue,
+        apikey: process.env.VUE_APP_ALPHA_VANTAGE_APIKEY,
+      };
+      axios
+        .get(urlBase, { params })
+        .then((res) => {
+          console.log(res);
+          this.searchResults = res.data.bestMatches;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+.autocomplete-list {
+    width: 300px;
+    margin: 0 auto;
+}
+.autocomplete-input {
+    padding: 10px;
+    width: 300px;
+    border-radius: 2px;
+    border: 1px solid black;
+}
+.autocomplete-result {
+    display: flex;
+    justify-content: space-between;
+    font-size: 0.90rem;
+    width: 300px;
+    border: 1px solid black;
+    padding: 3px;
+}
 </style>
